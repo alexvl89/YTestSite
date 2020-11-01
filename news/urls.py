4dcebea0c6b87175939b393f9ed0e2b918887106
alexvl89 <alexvl89.ru@gmail.com>
2020-11-01 20:57:33 +0300

    Добавлена маршрутизация

---
+++ news/urls.py
@@ -0,0 +1,12 @@
+#Маршрутизация на http://127.0.0.1/news/
+from django.urls import path
+
+#импортируем из views.py написанные функции
+from .views import *
+
+#указывает маршрут на функцию index, test
+urlpatterns=[
+    path('',index),
+    path('test/',test),
+
+]
